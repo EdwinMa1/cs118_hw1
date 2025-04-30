3db5f3636b6369df4ed654d381729857e4777ef3
Katelyn Chenqi <kchenqi@uci.edu>
2025-04-29 20:13:15 -0700

    trash bag particles.

--- VrProject/Assets/Player.cs
+++ VrProject/Assets/Player.cs
@@ -37,7 +37,7 @@ public class Player : MonoBehaviour
 
         mouse_x = Mathf.Clamp(mouse_x, -90f, 90f);
         transform.rotation = Quaternion.Euler(mouse_x, mouse_y, 0);
-        rb.velocity = inputH* orientation.right * speed +inputV * orientation.forward * speed;
+        rb.linearVelocity = inputH* orientation.right * speed +inputV * orientation.forward * speed;
 // rb.velocity = new Vector3(inputH * orientation.right * speed, rb.velocity.y, inputV * orientation.forward * speed);// * speed;
         orientation.rotation = Quaternion.Euler(0, mouse_y, 0);
 
