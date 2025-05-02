36b7bc751e8815ef2d7195298b9b6942be055851
Edwin Ma <edwinma707@gmail.com>
2025-05-02 15:19:11 -0700

    Staircase comes out when inspecting toilet

--- VrProject/Assets/StairRamp.cs
+++ VrProject/Assets/StairRamp.cs
@@ -2,11 +2,19 @@ using UnityEngine;
 public class StairRamp : MonoBehaviour
 {
     public Animator anim;
+    public Animator toiletAnim;
     private bool played = false;
 
+    void Start()
+{
+        // Hard-code position to match animation's frame 0
+        transform.position = new Vector3(25.32871f, 9.569587f, 51.03f); // Replace with actual "in" coordinates
+    }
     void Update()
     {
-        if (!played && Input.GetKeyDown(KeyCode.Space))
+        AnimatorStateInfo toiletState = toiletAnim.GetCurrentAnimatorStateInfo(0);
+
+        if (!played && toiletState.IsName("OpenLid"))
         {
             anim.SetTrigger("ComeOut");
             played = true; // block further triggers
