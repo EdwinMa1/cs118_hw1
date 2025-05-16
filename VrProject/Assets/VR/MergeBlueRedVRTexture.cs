546718a88f37977caaf1c48d18f520a6778fab1c
Edwin Ma <edwinma707@gmail.com>
2025-05-15 18:21:50 -0700

    Added Red-Blue Anaglyph

--- VrProject/Assets/VR/MergeBlueRedVRTexture.cs
+++ VrProject/Assets/VR/MergeBlueRedVRTexture.cs
@@ -1,24 +1,54 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MergeBlueRedVRTexture : MonoBehaviour
+public class AnaglyphCombiner : MonoBehaviour
 {
+    public RenderTexture leftTexture;
+    public RenderTexture rightTexture;
+    public RawImage outputImage;
 
-    public RawImage output;
-    public RenderTexture LeftEye, RightEye;
+    private Texture2D leftTex2D, rightTex2D, combinedTex;
 
-    public Shader removeRedShader;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        int width = leftTexture.width;
+        int height = leftTexture.height;
+
+        leftTex2D = new Texture2D(width, height, TextureFormat.RGB24, false);
+        rightTex2D = new Texture2D(width, height, TextureFormat.RGB24, false);
+        combinedTex = new Texture2D(width, height, TextureFormat.RGB24, false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
-        
+        // Read left texture
+        RenderTexture.active = leftTexture;
+        leftTex2D.ReadPixels(new Rect(0, 0, leftTexture.width, leftTexture.height), 0, 0);
+        leftTex2D.Apply();
+
+        // Read right texture
+        RenderTexture.active = rightTexture;
+        rightTex2D.ReadPixels(new Rect(0, 0, rightTexture.width, rightTexture.height), 0, 0);
+        rightTex2D.Apply();
+
+        RenderTexture.active = null;
+
+        // Combine into anaglyph
+        Color[] leftPixels = leftTex2D.GetPixels();
+        Color[] rightPixels = rightTex2D.GetPixels();
+        Color[] finalPixels = new Color[leftPixels.Length];
+
+        for (int i = 0; i < leftPixels.Length; i++)
+        {
+            float r = leftPixels[i].r;
+            float g = rightPixels[i].g;
+            float b = rightPixels[i].b;
+            finalPixels[i] = new Color(r, g, b);
+        }
+
+        combinedTex.SetPixels(finalPixels);
+        combinedTex.Apply();
+
+        outputImage.texture = combinedTex;
     }
 }
