b001c2799725518fcfbaad0564c195a0a8d94bc8
Edwin Ma <edwinma707@gmail.com>
2025-05-23 00:29:56 -0700

    Stair sound implemented, Bee volume reduced

--- VrProject/Assets/StairRamp.cs
+++ VrProject/Assets/StairRamp.cs
@@ -7,9 +7,11 @@ public class StairRamp : MonoBehaviour
     private bool played = false;
 
     void Start()
-{
+    {
         // Hard-code position to match animation's frame 0
         transform.position = new Vector3(25.32871f, 9.569587f, 51.03f); // Replace with actual "in" coordinates
+        audioSource.playOnAwake = false;
+        audioSource.Stop();
     }
     void Update()
     {
@@ -18,6 +20,7 @@ public class StairRamp : MonoBehaviour
         if (!played && toiletState.IsName("OpenLid"))
         {
             anim.SetTrigger("ComeOut");
+            audioSource.Play();
             played = true; // block further triggers
         }
     }
